ab9f4177c1704bc3352a61878bbaea160f425e57
EmreOTU <dev.emre17@gmail.com>
2025-01-21 19:47:11 +0000

    Enhance Node class

--- src/Trees/binarySearchTree/Node.java
+++ src/Trees/binarySearchTree/Node.java
@@ -2,11 +2,11 @@ package Trees.binarySearchTree;
 
 public class Node {
 
-    int data;
-    Node left;
-    Node right;
+    int data; // The value stored in the node
+    Node left; // Pointer to the left child node (smaller values)
+    Node right; // Pointer to the right child node (larger values)
 
     public Node(int data) {
-        this.data = data;
+        this.data = data; // Assign the given value to the node's data field
     }
 }
